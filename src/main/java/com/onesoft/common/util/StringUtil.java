59e09057dcade69796a07c5aa67cfcb9a5500567
KIMKYUSIK <onesoft@nanalsmi.com>
2026-02-23 14:58:49 +0900

    로거 사용을 위한 추가

--- src/main/java/com/onesoft/common/util/StringUtil.java
+++ src/main/java/com/onesoft/common/util/StringUtil.java
@@ -1,9 +1,16 @@
 package com.onesoft.common.util;
 
-public class StringUtil {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class StringUtil {
+	// 클래스 단위 Logger 생성
+    private static final Logger logger = LoggerFactory.getLogger(StringUtil.class);
+    
     public static boolean isEmpty(String str) {
-        return str == null || str.isEmpty();
+        boolean result = str == null || str.isEmpty();
+        logger.debug("isEmpty called with: {}, result: {}", str, result);
+        return result;
     }
 
     public static boolean isNotEmpty(String str) {
